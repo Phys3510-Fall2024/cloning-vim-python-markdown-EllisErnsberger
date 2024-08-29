68c40ea3f3f95a275d0571687d87c812658bd912
EllisErnsberger <ellisernsberger@my.unt.edu>
2024-08-29 09:27:45 -0500

    completed hello-world assignment

---
+++ .ipynb_checkpoints/hello-world-checkpoint.py
@@ -0,0 +1,4 @@
+# This program asks the user for their name and interest and prints the result to screen 
+user_name = input("Ellis Ernsberger") 
+user_interest = input("Astrophysics")
+print("My name is " + user_name + ", and I like " + user_interest + ".")
